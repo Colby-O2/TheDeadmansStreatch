e3af3b82d934df4ce8972f320725d9d02b2647e2
Colby-O2 <cokeefe919@gmail.com>
2026-04-05 22:00:14 -0400

    Delta time Camera fix

--- Assets/Scripts/Runtime/Polaroid/CameraMomentum.cs
+++ Assets/Scripts/Runtime/Polaroid/CameraMomentum.cs
@@ -25,11 +25,11 @@ namespace ColbyO.Untitled.Polaroid
             Vector3 r = Vector3.ProjectOnPlane(transform.InverseTransformDirection(_prevRotation), Vector3.forward);
             Vector2 dir = new Vector2(r.x, r.y).normalized;
             Vector2 imageOffset = dir * (delta * UTGameManager.Preferences.PolaroidCameraShakeImageMag);
-            _imagePosition += imageOffset * Time.deltaTime;
+            _imagePosition += imageOffset;
             _imagePosition = Vector2.Lerp(_imagePosition, Vector2.zero, Time.deltaTime * UTGameManager.Preferences.PolaroidCameraShakeRestorationRate);
             _viewFinderImage.anchoredPosition = _imagePosition;
             Vector2 crosshairOffset = dir * (delta * UTGameManager.Preferences.PolaroidCameraShakeCrosshairMag);
-            _crosshairPosition += crosshairOffset * Time.deltaTime;
+            _crosshairPosition += crosshairOffset;
             _crosshairPosition = Vector2.Lerp(_crosshairPosition, Vector2.zero, Time.deltaTime * UTGameManager.Preferences.PolaroidCameraShakeRestorationRate);
             _viewFinderCrosshair.anchoredPosition = _crosshairPosition;
             _prevRotation = transform.forward;
